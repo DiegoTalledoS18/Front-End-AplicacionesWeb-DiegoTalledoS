3fb9bfa2384d43b38f08c949e8889c9bb0443dea
unknown <diegotasa35@hotmail.com>
2022-09-08 18:28:43 -0500

    form complete

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,173 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import { ref } from "vue";
+export default {
+  data(){
+    return {
+      selectedCategories: null,
+      codigo:"",
+      nombre:"",
+      abreviatura:"",
+      Ruc:0,
+      fecha: null,
+      categories: [
+        {name: 'Lacteos', code: 'NY'},
+        {name: 'verduras', code: 'RM'},
+        {name: 'Bebidas', code: 'LDN'},
+        {name: 'Alcohol', code: 'IST'},
+        {name: 'Carnes', code: 'PRS'}
+      ],
+      estados: [ "Activo","Suspendido","De baja"],
+      estadoSeleccionado: ref("true"),
+      price: ref(3),
+      color: '1976D2',
+      impuesto:false,
+      descriptionValue:""
+    }
+  },
+  methods:{
+    clean(){
+      //Limpiar funcion
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <form action="">
 
-    <div class="wrapper">
-      <HelloWorld msg="Cree un proyecto con vue!" />
+
+    <div class="card">
+
+      <div class="field grid">
+        <label for="code" class="col-12 mb-2 md:col-1 md:mb-0" >Codigo* </label>
+        <div class="col-12 md:col-10">
+          <input-text id="code" v-model="codigo"/>
+          <Button class="ml-3" label="Validar"/>
+        </div>
+
+      </div>
+      <div class="field grid">
+
+        <label for="nombre" class="col-12 mb-2 md:col-1 md:mb-0" >Nombre*</label>
+        <div class="col-12 md:col-10">
+          <input-text id="nombre" placeholder="Solo letras y numeros" v-model="nombre"></input-text>
+        </div>
+
+      </div>
+
+      <div class="field grid">
+
+        <label for="abreviatura" class="col-12 mb-2 md:col-1 md:mb-0" >Abreviatura*</label>
+        <div class="col-12 md:col-10">
+          <input-text id="abreviatura" placeholder="Solo letras " v-model="abreviatura"></input-text>
+        </div>
+
+      </div>
+
+      <!--div class="field grid">
+        <label for="Telefono" class="col-12 mb-2 md:col-1 md:mb-0">Telefono*</label>
+        <div class="col-12 md:col-10">
+          <input-Mask id="Telefono" v-model="value" mask="a9-99999a" />
+        </div-->
+
+      <div class="field grid">
+
+        <label for="RUC" class="col-12 mb-2 md:col-1 md:mb-0" >RUC proveedor*</label>
+        <div class="col-12 md:col-10">
+          <input-text id="RUC" type="number" placeholder="Solo numeros " ></input-text>
+        </div>
+
+      </div>
+
+      <div class="field grid">
+
+        <label for="Precio" class="col-12 mb-2 md:col-1 md:mb-0" >Precio*</label>
+        <div class="col-12 md:col-10">
+          <input-text id="Precio" type="number" v-model="price" ></input-text>
+        </div>
+
+      </div>
+
+      <div class="field grid">
+
+        <label for="Categoria" class="col-12 mb-2 md:col-1 md:mb-0" >Categoria*</label>
+        <div class="col-12 md:col-10">
+          <pv-dropdown v-model="selectedCategories" :options="categories" optionLabel="name" placeholder="Select a categorie"></pv-dropdown>
+        </div>
+
+      </div>
+
+      <div class="field grid">
+        <label for="LabelColor" class="col-12 mb-2 md:col-1 md:mb-0" >Color de etiqueta*</label>
+        <div class="col-12 md:col-10">
+          <pv-ColorPicker id="LabelColor" v-model="color" />
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="calendario" class="col-12 mb-2 md:col-1 md:mb-0">Fecha*</label>
+        <div class="field col-12 md:col-10">
+          <Calendar id="calendario" inputId="icon" v-model="fecha" :showIcon="true" />
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="" class="col-12 mb-2 md:col-1 md:mb-0">Estado*</label>
+        <!--div class="field-radiobutton mr-2" v-for="estado in estados">
+          <RadioButton inputId="estado" name="estado" v-model="estadoSeleccionado"/>
+          <label for="estado">{{ estado }}</label>
+        </div>
+        <p>Estado: {{estadoSeleccionado}}</p-->
+        <div class="field-radiobutton mr-2">
+          <RadioButton inputId="estado1" name="estado" value="Activo" v-model="estadoSeleccionado" />
+          <label for="estado1">Activo</label>
+        </div>
+        <div class="field-radiobutton mr-2">
+          <RadioButton inputId="estado2" name="estado" value="Suspendio" v-model="estadoSeleccionado" />
+          <label for="estado2">Suspendio</label>
+        </div>
+        <div class="field-radiobutton mr-2">
+          <RadioButton inputId="estado3" name="estado" value="De baja" v-model="estadoSeleccionado" />
+          <label for="estado3">De baja</label>
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="impuesto" class="col-12 mb-2 md:col-1 md:mb-0">Afecta a impuesto*</label>
+        <div class="col-12 md:col-10">
+          <div class="field-checkbox">
+            <Checkbox id="impuesto" v-model="impuesto" :binary="true" />
+            <label for="binary">{{impuesto}}</label>
+          </div>
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="Foto" class="col-12 mb-2 md:col-1 md:mb-0">Foto*</label>
+        <div class="col-12 md:col-10">
+
+        </div>
+      </div>
+
+      <div class="field grid">
+        <label for="descripcion" class="col-12 mb-2 md:col-1 md:mb-0">Descripcion*</label>
+        <div class="col-12 md:col-10">
+          <Textarea id="descripcion" v-model="descriptionValue" :autoResize="true" rows="5" cols="30" />
+        </div>
+      </div>
+
+      <div class="field grid">
+        <div class="flex justify-content-center flex-wrap mb-2 md:mb-5">
+          <Button class="flex align-items-center justify-content-center m-2" >Grabar</Button>
+          <Button class="flex align-items-center justify-content-center m-2" @click="clean" >Limpiar</Button>
+        </div>
+      </div>
+
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  </form>
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,36 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import InputNumber from 'primevue/inputnumber';
+import InputMask from 'primevue/inputmask';
+import Dropdown from 'primevue/dropdown';
+import ColorPicker from 'primevue/colorpicker';
+import Calendar from 'primevue/calendar';
+import RadioButton from 'primevue/radiobutton';
+import Checkbox from 'primevue/checkbox';
+import Textarea from 'primevue/textarea';
 
-import './assets/main.css'
 
-createApp(App).mount('#app')
+import "primevue/resources/themes/bootstrap4-light-blue/theme.css"       //theme
+import "primevue/resources/primevue.min.css"                 //core css
+import "primeicons/primeicons.css"                         //icons
+import "/node_modules/primeflex/primeflex.css"
+const app = createApp(App);
+
+
+app.use(PrimeVue);
+app.component('Button', Button);
+app.component('InputText', InputText);
+app.component('InputNumber', InputNumber);
+app.component('InputMask', InputMask);
+app.component('pv-dropdown', Dropdown);
+app.component('pv-ColorPicker', ColorPicker);
+app.component('Calendar', Calendar);
+app.component('RadioButton', RadioButton);
+app.component('Checkbox', Checkbox);
+app.component('Textarea', Textarea);
+
+
+app.mount('#app')
